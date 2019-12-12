5ad8458cbe1cf8b12018eafa84535e33a8900163
shiv <shivssharma1990@gmail.com>
2019-12-12 14:55:32 +0530

    get my secret santa done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,6 @@
 const userModel = require('../model/user.model');
+const _ = require('lodash');
+const fs = require('fs');
 
 module.exports.addOneUser = (req, res) => {
 	const User = new userModel({
@@ -26,9 +28,9 @@ module.exports.addmanyUser = (req, res) => {
 	usersArray.forEach((user) => {
 		const User = new userModel({
 			email: user
-        });
-        
-        User.save()
+		});
+
+		User.save();
 	});
 
 	return res.send({
@@ -38,19 +40,58 @@ module.exports.addmanyUser = (req, res) => {
 };
 
 module.exports.getAllUsers = (req, res) => {
-    userModel.find({}, (err, usersFound) => {
-        if (err) {
-            return res.status(500).send({
-                error: true,
-                message: 'Something went wrong while getting users',
-                data: err
-            })
-        }
+	userModel.find({}, (err, usersFound) => {
+		if (err) {
+			return res.status(500).send({
+				error: true,
+				message: 'Something went wrong while getting users',
+				data: err
+			});
+		}
 
-        return res.status(200).send({
-            error: false,
-            message: usersFound.length ? 'users found' : 'No user available',
-            data: usersFound
-        })
-    })
-}
+		return res.status(200).send({
+			error: false,
+			message: usersFound.length ? 'users found' : 'No user available',
+			data: usersFound
+		});
+	});
+};
+
+module.exports.execute = (req, res) => {
+	userModel.find({}, (err, usersFound) => {
+		const shuffledUsers = _.shuffle(usersFound);
+		const pairedUsers = _.chunk(shuffledUsers, 2);
+
+		fs.writeFileSync('matching.json', JSON.stringify({ users: pairedUsers }));
+
+		res.send({
+			error: false,
+			message: 'Operation executed successfully'
+		});
+	});
+};
+
+module.exports.getMySecretSanta = (req, res) => {
+	const users = JSON.parse(fs.readFileSync('matching.json')).users;
+
+	users.forEach((userArr) => {
+		if (userArr[0].email === req.body.email) {
+			if (userArr.length === 1) {
+				return res.status(200).send({
+					error: false,
+					message: 'Sorry you have no match in our system'
+				});
+			}
+
+			return res.status.send({
+				error: false,
+				message: `Your secret santa is ${userArr[1].email}`
+			});
+		} else if (userArr[1].email === req.body.email) {
+			return res.status(200).send({
+				error: false,
+				message: `Your secret santa is ${userArr[0].email}`
+			});
+		}
+	});
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require('../controller/user.controller')
+const userController = require('../controller/user.controller');
 
-router.post("/addOne", userController.addOneUser);
+router.post('/addOne', userController.addOneUser);
 
-router.post("/addMany", userController.addmanyUser);
+router.post('/addMany', userController.addmanyUser);
 
-router.get("/getAll", userController.getAllUsers);
+router.get('/getAll', userController.getAllUsers);
+
+router.get('/execute', userController.execute);
+
+router.post('/getMySecretSanta', userController.getMySecretSanta);
 
 module.exports = router;
